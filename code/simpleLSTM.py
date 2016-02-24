241a7f12ba5fe1c9f76b831e69d64c98eec8f7f2
Alex Evert <alex.evert@student.gu.se>
2016-02-24 10:59:50 +0100

    Bugfix - Change embedding_size to match vectors
    The glove vectors are 100-dimensional. One can of course
    alse switch glove file.

--- code/simpleLSTM.py
+++ code/simpleLSTM.py
@@ -11,7 +11,7 @@ from DataMan import DataMan
 
 # Hyper parameters 
 batch_size = 20
-hidden_layer_size = embedding_size = 200 
+hidden_layer_size = embedding_size = 100 
 number_of_layers = 2
 learning_rate = 1.0 
 init_range = 0.1
