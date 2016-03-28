a48d73cb20902c960688b2712c4210257308e619
Filip <filmarnil@gmail.com>
2016-03-28 14:53:25 +0200

    Calculate and plot accuracy. Also commented some code.

--- code/simpleLSTM/LSTMChar/train.py
+++ code/simpleLSTM/LSTMChar/train.py
@@ -9,7 +9,7 @@ import numpy as np
 import cPickle as pickle
 import hyperParams
 import DataManager 
-import plot
+import eval_plot
 import time
 import sys
 
@@ -58,11 +58,15 @@ class LSTM_Network(object):
         z = tf.matmul(output, w) + b # Add supports broadcasting over each row
 
         # Average negative log probability
-        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(z, tf.reshape(self._target, [-1]))
-        self._cost = cost = tf.reduce_sum(loss) / (batch_size * num_steps)
+        targets = tf.reshape(self._target, [-1])
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(z, targets)
+        self.cost = cost = tf.reduce_sum(loss) / (batch_size * num_steps)
+        # Accuracy is calculated by looking for each target in the top 5 most predicted
+        correct_preds = tf.nn.in_top_k(tf.nn.softmax(z), targets, 5) 
+        self.accuracy = tf.reduce_mean(tf.cast(correct_preds, tf.float32))
         
         if not training:
-            self._train_op = tf.no_op()
+            self.train_op = tf.no_op()
             return
 
         self._learning_rate = tf.Variable(config["learning_rate"], trainable=False)
@@ -72,21 +76,22 @@ class LSTM_Network(object):
         grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), config["gradient_clip"])
         # RMSProp training op
         optimizer = tf.train.RMSPropOptimizer(self._learning_rate)
-        self._train_op = optimizer.apply_gradients(zip(grads,tvars))
+        self.train_op = optimizer.apply_gradients(zip(grads,tvars))
 
     def set_learning_rate(self, sess, value):
         sess.run(tf.assign(self._learning_rate, value))
 
 def run_epoch(sess, net, data_man, data_set):
-    total_cost = 0.0
+    total_cost, total_acc = 0.0, 0.0
     state = sess.run(net.initial_state)
     for i, (x, y) in enumerate(data_man.batch_iterator(net.batch_size, net.num_steps, data_set)):
         # Input
         feed = { net._input : x, net._target : y , net.initial_state : state}
         # Calculate cost and train the network 
-        cost, state, _ = sess.run([net._cost, net.final_state, net._train_op], feed_dict=feed)
+        cost, state, acc,  _ = sess.run([net.cost, net.final_state, net.accuracy, net.train_op], feed_dict=feed)
+        total_acc += acc
         total_cost += cost
-    return total_cost / (i+1)
+    return total_cost / (i+1), total_acc / (i+1)
 
 def save_state(sess, saver, config):
     print("Saving model.")
@@ -98,7 +103,7 @@ def main():
 
     config = hyperParams.config
     data_man = DataManager.DataMan("data.txt", 0.05)
-    # Update config with information from DataMan
+    # Update config with information from data manager 
     data_params = { "word_to_id" : data_man.char_to_id, "id_to_word" : data_man.id_to_char,
             "vocab_size" : data_man.vocab_size}
     config.update(data_params)
@@ -120,24 +125,30 @@ def main():
         sess.run(init)
 
         max_epoch = config["max_epoch"]
-        cost_train, cost_valid = [], []
+        cost_train, cost_valid, accuracy = [], [], []
         print("Training.")
         for i in range(0, max_epoch):
             print("\r{}% done".format(int(i/max_epoch * 100)), end="")
             sys.stdout.flush()
 
+            # Code needed for learning rate decay
             if i > config["decay_start"]:
                 decay = config["learning_decay"] ** (i - config["decay_start"])
                 train_net.set_learning_rate(sess, config["learning_rate"] * decay)
 
-            cost_t = run_epoch(sess, train_net, data_man, DataManager.TRAIN_SET)
+            # Train the network and evaluate it
+            cost_t, _ = run_epoch(sess, train_net, data_man, DataManager.TRAIN_SET)
             cost_train.append(cost_t)
-            cost_v = run_epoch(sess, val_net, data_man, DataManager.VALID_SET)
+            cost_v, acc = run_epoch(sess, val_net, data_man, DataManager.VALID_SET)
             cost_valid.append(cost_v)
-
-            plot.create_plots(range(i+1), cost_train, cost_valid)
+            accuracy.append(acc)
+            
+            # Plot results
+            eval_plot.plot_costs(range(i+1), cost_train, cost_valid)
+            eval_plot.plot_accuracy(range(i+1), accuracy)
         print("\r100% done")
 
+        # Save and exit
         save_state(sess, saver, config)
         sess.close()
         print("--- {} seconds ---".format(round(time.time() - start_time, 2)))
