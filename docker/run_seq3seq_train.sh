be1dd58fc16721f29f31714ca5e9c1e6d255ab8e
Alex Evert <alex.evert@student.gu.se>
2016-04-12 00:11:43 +0200

    Use our docker image.

--- docker/run_seq3seq_train.sh
+++ docker/run_seq3seq_train.sh
@@ -3,5 +3,9 @@ source_dir=$(pwd)/code
 data_dir=~/data
 output_dir=$(pwd)/seq3seq
 
+echo "Building docker image"
+docker build -t genhead/seq3seq:gpu -f ./docker/Dockerfile . 
+
+echo "Start roughly one hour training"
 echo $source_dir
-./docker/docker_run_gpu.sh --rm -t -w="/" -v $output_dir:/seq3seq -v $data_dir:/data -v $source_dir:/code b.gcr.io/tensorflow/tensorflow:0.7.1-gpu /code/seq3seq/train.sh
+./docker/docker_run_gpu.sh --rm -t -w="/" -v $output_dir:/seq3seq -v $data_dir:/data -v $source_dir:/code genhead/seq3seq:gpu -gpu /code/seq3seq/train.sh
