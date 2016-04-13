643fdb02ff9094f4ae503499aadfedb4c80d1ab9
Alex Evert <alex.evert@student.gu.se>
2016-04-13 21:32:04 +0200

    Reduce model size

--- code/seq3seq/translate.py
+++ code/seq3seq/translate.py
@@ -52,10 +52,10 @@ tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
                           "Clip gradients to this norm.")
 tf.app.flags.DEFINE_integer("batch_size", 6,
                             "Batch size to use during training.")
-tf.app.flags.DEFINE_integer("size", 1024, "Size of each model layer.")
+tf.app.flags.DEFINE_integer("size", 300, "Size of each model layer.")
 tf.app.flags.DEFINE_integer("num_layers", 3, "Number of layers in the model.")
-tf.app.flags.DEFINE_integer("article_vocab_size", 40000, "Article vocabulary size.")
-tf.app.flags.DEFINE_integer("title_vocab_size", 40000, "Title vocabulary size.")
+tf.app.flags.DEFINE_integer("article_vocab_size", 20000, "Article vocabulary size.")
+tf.app.flags.DEFINE_integer("title_vocab_size", 20000, "Title vocabulary size.")
 tf.app.flags.DEFINE_string("data_dir", ".", "Data directory")
 tf.app.flags.DEFINE_string("article_file", "articles.txt",
                            "file containing the articles (relative to data_dir)")
